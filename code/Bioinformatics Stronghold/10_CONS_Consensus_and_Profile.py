447f7f7263ba8f1e88c91eaac7e9a5ff2d635b14
Jonathan Peh <jonathan.peh.qj@gmail.com>
2025-01-21 17:56:00 +0800

    Update 10_CONS_Consensus_and_Profile.py

--- code/Bioinformatics Stronghold/10_CONS_Consensus_and_Profile.py
+++ code/Bioinformatics Stronghold/10_CONS_Consensus_and_Profile.py
@@ -1,9 +1,9 @@
 collectionlist = []
 read = ""
 
-ACGTtoInd = {"A":0, "C":1, "G":2, "T":3}
+ACGTtoInd = {"A":0, "C":1, "G":2, "T":3} # Dictionary used as looping can be done over the indices, but not the nucleotides (Unequal difference in ASCII values of characters)
 
-with open("rosalind_cons.txt","r") as f:
+with open("rosalind_cons.txt","r") as f: # Reads files. Each element in collection list is a string of a read
     while True:
         l = f.readline().rstrip()
         if len(l) == 0:
@@ -16,29 +16,29 @@ with open("rosalind_cons.txt","r") as f:
         else:
             read += l
 
-collection_matrix = [0]*len(collectionlist[0])
+collection_matrix = [0]*len(collectionlist[0]) # Initialises matrix. Note: [[a]*b]*c creats c exact duplicates of [a]*b. Even ADDRESSES are duplicated. Took me a while to realise
 for i in range(len(collection_matrix)):
     collection_matrix[i] = [0]*4
 
-for i in range(len(collectionlist[0])):
+for i in range(len(collectionlist[0])): # This collection matrix is  essentially the required consensus matrix, with coloumns nested in rows
     for j in range(len(collectionlist)):
         for k in ACGTtoInd:
             if collectionlist[j][i] == k:
                 collection_matrix[i][ACGTtoInd[k]] +=1
 
 
-MaxIndices = [0]*len(collection_matrix)
+MaxIndices = [0]*len(collection_matrix) # Finds index of most prominent nucelotide in each position
 for i in range(len(collection_matrix)):
     MaxIndex = collection_matrix[i].index(max(collection_matrix[i]))
     MaxIndices[i] = MaxIndex
 
-String = ""
+String = "" # Iterates through MaxIndices to convert index to nucleotide
 for i in range(len(MaxIndices)):
     for key in ACGTtoInd:
         if ACGTtoInd[key] == MaxIndices[i]:
            String += key
 
-
+#The rest is just presentation
 print("".join(String))
 transpose = list(zip(*collection_matrix))
 
